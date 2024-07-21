dd8d2c3d820a956458d5c2e6c8e1110c851e862a
Wenderson-Arlindo <wenderson250408@gmail.com>
2024-07-21 03:36:30 -0300

    Add files via upload

---
+++ curso em vídeo/aula02/ex004.py
@@ -0,0 +1,29 @@
+n = input('digite alguma coisa: ')
+
+print(' ')
+
+print('ta capitalizado:', n.istitle())
+
+print(' ')
+
+print('é maiúsculo:', n.isupper())
+
+print(' ')
+
+print('é minúsculo:', n.islower())
+
+print(' ')
+
+print('é alfabetico:', n.isalpha())
+
+print(' ')
+
+print('é numerico:', n.isnumeric())
+
+print(' ')
+
+print('é alfanumérico:', n.isalnum())
+
+print(' ')
+
+print('tem só espaço:', n.isspace())
