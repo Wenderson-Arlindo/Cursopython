1e394d5904d87387c408ec234200f08d6820e28c
Wenderson-Arlindo <wenderson250408@gmail.com>
2024-07-21 03:36:07 -0300

    ex003.py

---
+++ curso em vídeo/aula02/ex003.py
@@ -0,0 +1,9 @@
+n1 = int(input('informe um número: '))
+
+print(' ')
+
+n2 = int(input('informe outro número: '))
+
+print(' ')
+
+print('a soma entre {} e {} é igual a {}!!!'.format(n1, n2, (n1+n2)))
